b56106bd01392f3eaea606cb34e1819107bf10ac
Avichal Pandey <avichal.pandey@zalando.de>
2022-03-14 15:01:31 +0530

    Fetch contract ABIs and remove the data file (got too big)

--- src/ethscan_dump/ethscan_dump.clj
+++ src/ethscan_dump/ethscan_dump.clj
@@ -2,50 +2,63 @@
   (:import (org.jsoup Jsoup)))
 
 
-(def URL "https://etherscan.io/contractsVerified/")
+(def etherscan-url "https://etherscan.io/")
+(def contract-url (str etherscan-url "address/"))
+(def v-contracts-url (str etherscan-url "contractsVerified/"))
 
 
 (def page-size 100)
 
 
-(defn get-page [i]
-  (.get (Jsoup/connect (str URL i "?ps=" page-size))))
-
-
-(defn get-elems [page css]
-  (.select page css))
-
-
 (def result-set (atom {}))
 
 
+(defn fetch!
+  [url selector]
+  (-> url
+      (Jsoup/connect)
+      (.get)
+      (.select selector)))
 
-(defn fetch-verified-contracts! []
+
+(defn fetch-abi
+  [addr]
+  (-> (str contract-url "/" addr "#code")
+      (fetch! "#js-copytextarea2")
+      (.text)))
+
+(defn v-contract-addrs [rows]
+  (->> (for [r rows]
+         (for [cell (.select r "td")]
+           (.text cell)))
+       (first)
+       (partition-all 10)
+       (mapv (partial take 2))))
+
+;; TODO: make abi fetching concurrent
+(defn fetch-verified-contracts []
   (dotimes [i 5]
-    (let [html           (get-page (inc i))
-          rows           (get-elems html ".table > tbody")
-          contract-addrs (->> (for [r rows]
-                                (for [cell (get-elems r "td")]
-                                  (.text cell)))
-                              (first)
-                              (partition-all 10)
-                              (mapv (partial take 2)))]
+    (let [rows           (fetch! (str URL (inc i) "?ps=" page-size)
+                                 ".table > tbody")
+          contract-addrs (v-contract-addrs rows)]
       (doseq [[addr contract] contract-addrs]
-        (reset! result-set (merge @result-set {addr contract}))))))
+        (reset! result-set
+                (merge @result-set {addr
+                                    {:name contract
+                                     :abi  (fetch-abi addr)}}))))))
 
 
 
 (comment
 
-  ;; Get Rows
-  (let [html (get-page 2)
-        header (get-elems html ".table > thead > tr")]
-    (take 2 (clojure.string/split (.text (first header)) #" ")))
+  (fetch-abi "0x8551880f3FE9bd29aA6bB8D89DD6ffa36DB2c73A")
 
 
   ;; Should be 500
-  (do (fetch-verified-contracts!)
-      (= (count  @result-set) 500))
+  (do (fetch-verified-contracts)
+      (= (count  @result-set) 508))
+
+  (first @result-set)
 
 
   ;; Dump verified contracts in a file
