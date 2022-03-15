b4c5b3716ac3fc06fde6b66c5772546e18ca2a60
Avichal Pandey <avichal.pandey@zalando.de>
2022-03-15 13:25:35 +0530

    Fetch contract name tags from accounts page

--- src/ethscan_dump/ethscan_dump.clj
+++ src/ethscan_dump/ethscan_dump.clj
@@ -5,6 +5,7 @@
 (def etherscan-url "https://etherscan.io/")
 (def contract-url (str etherscan-url "address/"))
 (def v-contracts-url (str etherscan-url "contractsVerified/"))
+(def accounts-url (str etherscan-url "accounts"))
 
 
 (def page-size 100)
@@ -27,21 +28,41 @@
       (fetch! "#js-copytextarea2")
       (.text)))
 
-(defn v-contract-addrs [rows]
+
+(defn processed-html-table
+  [rows col-count col-selector]
   (->> (for [r rows]
          (for [cell (.select r "td")]
            (.text cell)))
        (first)
-       (partition-all 10)
-       (mapv (partial take 2))))
+       (partition-all col-count)
+       (mapv col-selector)))
+
+
+(defn fetch-name-tags []
+  (dotimes [i 50]
+    (let [rows          (fetch!  (str accounts-url "/" (inc i) "?ps=" page-size)
+                                 "table > tbody")
+          accs          (processed-html-table
+                         rows
+                         6
+                         #(->> % (take 3) (drop 1)))
+          filtered-accs (filter #(-> % second empty? not) accs)]
+      (doseq [[addr name] filtered-accs]
+        (reset! result-set
+                (merge @result-set {addr {:name name}}))))))
+
+
 
 ;; TODO: make abi fetching concurrent
 (defn fetch-verified-contracts []
   (dotimes [i 5]
-    (let [rows           (fetch! (str URL (inc i) "?ps=" page-size)
-                                 ".table > tbody")
-          contract-addrs (v-contract-addrs rows)]
-      (doseq [[addr contract] contract-addrs]
+    (let [rows  (fetch! (str v-contracts-url (inc i) "?ps=" page-size)
+                        ".table > tbody")
+          addrs (processed-html-table rows
+                                      10
+                                      (partial take 2))]
+      (doseq [[addr contract] addrs]
         (reset! result-set
                 (merge @result-set {addr
                                     {:name contract
@@ -51,15 +72,38 @@
 
 (comment
 
+  (fetch! (str v-contracts-url (inc 1) "?ps=" page-size)
+          ".table > tbody")
+
   (fetch-abi "0x8551880f3FE9bd29aA6bB8D89DD6ffa36DB2c73A")
 
+  (count @result-set)
+
+  (processed-html-table
+   (fetch! accounts-url
+           "table > tbody")
+   6
+   #(->> %
+         (take 3)
+         (drop 1)))
 
   ;; Should be 500
   (do (fetch-verified-contracts)
       (= (count  @result-set) 508))
 
-  (first @result-set)
+  (fetch-name-tags)
 
+  (count @result-set)
+
+
+  (->> (for [r
+             (fetch! accounts-url
+                     "table > tbody")]
+         (for [cell (.select r "td")]
+           (.text cell)))
+       (first)
+       (partition-all 6)
+       (mapv (partial take 2)))
 
   ;; Dump verified contracts in a file
   (spit "verified_contracts.edn" (pr-str @result-set))
